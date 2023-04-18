c9f75a8a9e06eb8b1ce888fbc270f41ac14057ba
Надежда Белова <belovanadezhda1983@gmail.com>
2023-04-18 11:32:58 +0300

    PermissionDenied 2rewiu

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,8 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from rest_framework.exceptions import PermissionDenied
-
 from django.shortcuts import get_object_or_404
-
 from posts.models import Group, Post
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 
@@ -13,6 +11,7 @@ API_403 = PermissionDenied(
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -31,10 +30,12 @@ class PostViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
